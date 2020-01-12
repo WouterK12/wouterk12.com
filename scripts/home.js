5f37a45908040a5fdc1e6a55eb596f9cf462c10f
Wouter Koopman <wouter.koopman@student.fontys.nl>
2020-01-12 13:17:43 +0100

    Added hamburger menu to tabs (responsive)

--- scripts/home.js
+++ scripts/home.js
@@ -1,35 +1,84 @@
 ScrollReveal().reveal("section");
 
-// const nav = document.querySelector("nav");
 const drop = document.querySelector(".drop .icon");
 const scrollTo = document.querySelector(".scroll");
 
 const logo = document.querySelector(".start .logo");
 const logoNav = document.querySelector("nav .logo.nav");
 
+const ham = document.querySelector(".hamburger");
+const tabs = document.querySelector(".tabs");
+
+// SHOW OR HIDE TABS ON @MEDIA CHANGE
+var mediaQuery = window.matchMedia("(min-width: 900px)");
+OnMediaChange(mediaQuery);
+mediaQuery.addListener(OnMediaChange);
+
+function OnMediaChange(mediaQuery) {
+  if (mediaQuery.matches) {
+    ShowTabs();
+  } else {
+    HideTabs();
+  }
+}
+
+// DROP BUTTON
 drop.addEventListener("click", () => {
   scrollTo.scrollIntoView({ behavior: "smooth", block: "start" });
 });
 
-$(window).scroll(function() {
-  scrollToPos = logo.getBoundingClientRect();
-  if (scrollToPos.top <= 0) {
-    logoNav.style.opacity = "1";
-    logo.style.opacity = "0";
-
-    drop.style.opacity = "0";
-    drop.style.zIndex = "-1";
-    drop.style.pointerEvents = "none";
-
-    // nav.classList.add("dark");
+ham.addEventListener("click", () => {
+  GetLogoInfo();
+  if (tabs.style.opacity != 0) {
+    if (scrollToPos.top <= 0) {
+      ShowNavLogo();
+    }
+    HideTabs();
   } else {
-    logoNav.style.opacity = "0";
-    logo.style.opacity = "1";
+    HideNavLogo();
+    ShowTabs();
+  }
+});
 
-    drop.style.opacity = "1";
+function HideTabs() {
+  tabs.style.opacity = 0;
+  tabs.style.pointerEvents = "none";
+  tabs.style.transform = "translateX(-100%)";
+}
+function ShowTabs() {
+  tabs.style.opacity = 1;
+  tabs.style.pointerEvents = "unset";
+  tabs.style.transform = "unset";
+}
+
+function HideNavLogo() {
+  logoNav.style.opacity = 0;
+  logoNav.style.zIndex = -1;
+}
+function ShowNavLogo() {
+  logoNav.style.opacity = 1;
+  logoNav.style.zIndex = "unset";
+}
+
+function GetLogoInfo() {
+  scrollToPos = logo.getBoundingClientRect();
+}
+
+$(window).scroll(function() {
+  GetLogoInfo();
+  if (scrollToPos.top <= 0) {
+    ShowNavLogo();
+    logo.style.opacity = 0;
+
+    drop.style.opacity = 0;
+    drop.style.zIndex = -1;
+    drop.style.pointerEvents = "none";
+  } else {
+    HideNavLogo();
+    logo.style.opacity = 1;
+
+    drop.style.opacity = 1;
     drop.style.zIndex = "unset";
     drop.style.pointerEvents = "unset";
-
-    // nav.classList.remove("dark");
   }
 });
