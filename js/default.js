f3e1667bc99722bcedb3e5579a47b18f17554718
Wouter Koopman <34215333+WouterK12@users.noreply.github.com>
2020-07-10 14:49:31 +0200

    Added responsiveness

--- js/default.js
+++ js/default.js
@@ -1,6 +1,11 @@
+const body = document.querySelector("body");
+
 const nav = document.querySelector("nav");
 const navBackground = document.querySelector("nav .background");
 const navLogo = document.querySelector("nav .logo");
+
+const hamburger = document.querySelector("nav .hamburger img");
+const closeHamburger = document.querySelector("nav .hamburger span");
 
 const url = "https://api.mcsrvstat.us/2/play.wouterk12.com";
 $.getJSON(url, function (data) {
@@ -8,3 +13,20 @@ $.getJSON(url, function (data) {
     $(".maptab").css("display", "flex");
   }
 });
+
+hamburger.addEventListener("click", OpenMenu);
+closeHamburger.addEventListener("click", CloseMenu);
+
+navLogo.addEventListener("click", function () {
+  window.location.href = "./";
+});
+
+function OpenMenu() {
+  nav.classList.add("max");
+  body.style.overflow = "hidden";
+}
+
+function CloseMenu() {
+  nav.classList.remove("max");
+  body.style.overflow = "";
+}
